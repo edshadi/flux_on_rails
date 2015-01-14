4298b8b1e913747c841648e99a6aa94b5612bdc2
Ed Shadi <edshadi@gmail.com>
2015-01-14 13:02:54 -0800

    add new line after requires

--- lib/generators/flux_on_rails/install_generator.rb
+++ lib/generators/flux_on_rails/install_generator.rb
@@ -23,16 +23,16 @@ module FluxOnRails
         manifest
       end
       def add_react_form_builder
-        insert_into_file js_manifest, "//= require form-for", :after => "react\n"
+        insert_into_file js_manifest, "//= require form-for\n", :after => "react\n"
         directory 'react-form-for-object', 'vendor/assets/javascripts/react-form-for-object'
         copy_file 'form-for.js', 'vendor/assets/javascripts/form-for.js'
       end
       def add_react
-        insert_into_file js_manifest, "//= require react", :after => "jquery_ujs\n"
+        insert_into_file js_manifest, "//= require react\n", :after => "jquery_ujs\n"
         copy_file 'react.js', 'vendor/assets/javascripts/react.js'
       end
       def add_flux_dispatcher
-        insert_into_file js_manifest, "//= require dispatcher", :after => "react\n"
+        insert_into_file js_manifest, "//= require dispatcher\n", :after => "react\n"
         copy_file 'dispatcher.js', 'vendor/assets/javascripts/dispatcher.js'
       end
       def add_flux_dirs
